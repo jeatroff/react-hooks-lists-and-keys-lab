d120acf2a35183e83ce39dadf85257c182bfba04
Jonathan Eatroff <jonathan.eatroff@gmail.com>
2022-12-11 21:56:46 -0500

    commit tree 28ccff9034e09d3cf063523290f9565062840c74

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,12 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav> {
+      links.map((name, index) =>
+        <a href={"#"+name} key={index}>{name}</a>
+      )
+    }
+    </nav>;
 }
 
 export default NavBar;
